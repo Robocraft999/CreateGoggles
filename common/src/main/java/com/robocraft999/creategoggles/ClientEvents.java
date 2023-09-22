de61543d52e14a26f8a2ef5824788d8d3c670aca
Robocraft999 <59525378+Robocraft999@users.noreply.github.com>
2023-09-22 17:04:36 +0200

    porting some more stuff

--- common/src/main/java/com/robocraft999/creategoggles/ClientEvents.java
+++ common/src/main/java/com/robocraft999/creategoggles/ClientEvents.java
@@ -3,7 +3,7 @@ package com.robocraft999.creategoggles;
 import com.robocraft999.creategoggles.item.goggle.GoggleArmorLayer;
 import com.robocraft999.creategoggles.item.modifier.ItemModifierManager;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
-import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry;
+import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
 import net.fabricmc.fabric.api.client.rendering.v1.LivingEntityFeatureRendererRegistrationCallback;
 import net.fabricmc.fabric.api.client.rendering.v1.LivingEntityFeatureRendererRegistrationCallback.RegistrationHelper;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
@@ -31,13 +31,16 @@ public class ClientEvents {
         }
     }
 
+    /*private static void onModelRegistry(ModelLoadingPlugin.Context out) {
+        var list = new ArrayList<PartialModel>();
+        out.addModels(list.stream().map(PartialModel::getLocation).toList());
+    }*/
+
     public static void register(){
         LivingEntityFeatureRendererRegistrationCallback.EVENT.register(ClientEvents::addEntityRenderLayers);
         ItemTooltipCallback.EVENT.register(ClientEvents::onTooltip);
-        ModelLoadingRegistry.INSTANCE.registerModelProvider((manager, out) -> {
-            out.accept(new ModelResourceLocation(CreateGoggles.MOD_ID + ":goggle#inventory"));
-        });
+        ModelLoadingPlugin.register(pluginContext -> pluginContext.addModels(new ModelResourceLocation(CreateGoggles.MOD_ID, "goggle", "inventory")));
+        //ModelLoadingPlugin.register(pluginContext -> pluginContext.addModels(ClientEvents.onModelRegistry()));
+        //ModelLoadingPlugin.register(ClientEvents::onModelRegistry);
     }
-
-
 }
