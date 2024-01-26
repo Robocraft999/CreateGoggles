65f9501efa22570e307d195b8015695a4d1d5215
Robocraft999 <59525378+Robocraft999@users.noreply.github.com>
2024-01-26 17:06:41 +0100

    cleanup

--- forge/src/main/java/com/robocraft999/creategoggles/forge/mixin/ClientEventsMixin.java
+++ forge/src/main/java/com/robocraft999/creategoggles/forge/mixin/ClientEventsMixin.java
@@ -10,7 +10,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.FluidState;
-import net.minecraftforge.client.event.EntityViewRenderEvent;
+import net.minecraftforge.client.event.ViewportEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -43,7 +43,7 @@ public class ClientEventsMixin {
             locals = LocalCapture.CAPTURE_FAILHARD,
             remap = false
     )
-    private static void onGetFogDensity(EntityViewRenderEvent.RenderFogEvent event, CallbackInfo ci, Camera camera, Level level,
+    private static void onGetFogDensity(ViewportEvent.RenderFog event, CallbackInfo ci, Camera camera, Level level,
                                         BlockPos blockPos, FluidState fluidState, Fluid fluid, Entity entity, ItemStack divingHelmet
     ){
         if (!divingHelmet.isEmpty() && FluidHelper.isLava(fluid) && divingHelmet.getItem() instanceof DivingHelmetItem && isNetheriteArmor(divingHelmet)){
