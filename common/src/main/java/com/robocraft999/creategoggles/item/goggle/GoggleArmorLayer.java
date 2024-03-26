a6db9691d00bf4feadd28515cdbcd53e05637c5b
Robocraft999 <59525378+Robocraft999@users.noreply.github.com>
2024-03-26 20:57:02 +0100

    some update fixes

--- common/src/main/java/com/robocraft999/creategoggles/item/goggle/GoggleArmorLayer.java
+++ common/src/main/java/com/robocraft999/creategoggles/item/goggle/GoggleArmorLayer.java
@@ -14,6 +14,7 @@ import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Pose;
+import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 
 public class GoggleArmorLayer<T extends LivingEntity, M extends EntityModel<T>> extends RenderLayer<T, M> {
